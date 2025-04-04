32d0901b73d33e788660d75e360d165ce2206fa9
huanghykris <huanghy0820@163.com>
2025-04-04 14:04:11 +0800

    Coivd_19

---
+++ Coivd_19-Prediction/README.md
@@ -0,0 +1,7 @@
+# Coivd_19-Prediction
+Regression prediction model
+
+Objectives:
+* Solve a regression problem with deep neural networks (DNN).
+* Understand basic DNN training tips.
+* Familiarize yourself with PyTorch.
---
+++ MachineTranslation/train.py
@@ -0,0 +1,28 @@
+import sys
+import pdb
+import pprint
+import logging
+import os
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+import numpy as np
+import tqdm.auto as tqdm
+from pathlib import Path
+from argparse import Namespace
+from fairseq import utils
+
+import matplotlib.pyplot as plt
+
+seed = 73
+random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+np.random.seed(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
